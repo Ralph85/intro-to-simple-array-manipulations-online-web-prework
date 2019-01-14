2473360be18515a9c12bc596ec884d5df916ad06
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-14 04:58:28 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,10 @@
+artists = ["Hozier", "Ariana Grande", "Usher"]
+next_artist = "Beyonce"
+artists.push(next_artist)
+# => ["Hozier", "Ariana Grande", "Usher", "Beyonce"]
+artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.pop
+# => "Cai Guo-Qiang"
+
+artists
+#=> ["Frida Kahlo", "Pablo Picasso"]
